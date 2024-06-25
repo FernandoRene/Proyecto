ba6f89a5e4f139233bdd561354f86de1fcc558f3
FernandoRene <lluscofernando1@gmail.com>
2024-06-24 22:54:10 -0400

    Creado el proyecto inicial

---
+++ src/main/java/com/demoblaze/BasePage.java
@@ -0,0 +1,12 @@
+package com.demoblaze;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+    protected final WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
